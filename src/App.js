bfe2239e6d8f3ecff14ff489177a0e76be7ad7a1
Emba Moussa <emba.moussa@eventinc.de>
2017-05-01 12:33:16 +0200

    integrar firebase y anadir autenticacion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({user});
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+
+  }
+
+  renderLoginButton () {
+    if (this.state.user) {
+      return (
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayNmae}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      )
+    } else {
+      return (
+        <button onClick={this.handleAuth}>Login con Google</button>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.renderLoginButton() }
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDxvxVZeIzJPeSOSfu7714qpsVenY5GTjM",
+  authDomain: "pseudogram-5575d.firebaseapp.com",
+  databaseURL: "https://pseudogram-5575d.firebaseio.com",
+  projectId: "pseudogram-5575d",
+  storageBucket: "pseudogram-5575d.appspot.com",
+  messagingSenderId: "578501539923"
+});
 ReactDOM.render(
   <App />,
   document.getElementById('root')
